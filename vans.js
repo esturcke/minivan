3669c54ad983c4297a26a4b5dddc2201e3c821aa
Erik J. Sturcke <esturcke@cisco.com>
2015-11-20 22:08:25 -0500

    Update to fit new data format

--- vans.js
+++ vans.js
@@ -1,4 +1,34 @@
-'use strict';
+'use strict'
+
+const MILES_PER_YEAR = 15000
+const TAX = 1.06
+
+// Dealership fees, defaulting to 800 if unknown.
+let fees = (car) => ({
+  'sport honda'                    : 299,
+  'mile one'                       : 299,
+  'mercedes-benz of silver spring' : 299,
+  'carmax'                         : 99,
+  'chantilly auto sales'           : 595,
+  'joyce koons'                    : 800,
+}[car.dealer] || 800)
+
+let process = (car) => {
+  // expand tags
+    car.tags.split("|").reduce((tags, tag) => {
+      let t = tag.split(":")
+     tags[t[0]] = t[1] === undefined ? 1 : t[1]
+     return tags
+    }, car)
+
+  car.fees  = fees(car)
+  car.price = car.quote || +car.price
+  car.total = (car.price - (car.discount||0) + car.fees) * TAX
+  car.miles = +car.miles
+  car.year  = +car.year
+}
+
+let cost_per_year = (car, total_miles) => car.total / (total_miles - car.miles) * MILES_PER_YEAR
 
 var margin = { top: 20, right: 20, bottom: 230, left: 60 },
     width  = 960 - margin.left - margin.right,
@@ -16,7 +46,7 @@ var yScale = d3.scale.linear().range([height, 0]),
 var yValue, yMap;
 
 function updateY(total_miles) {
-    yValue = function(d) { return d.price / (total_miles - d.miles) * 18000};
+    yValue = car => cost_per_year(car, total_miles)
     yMap   = function(d) { return yScale(yValue(d)) };
 
     svg.select("g .y.axis").call(yAxis);
@@ -31,8 +61,9 @@ function updateY(total_miles) {
 }
 
 // setup fill color
-var cValue = function(d) { return d.year },
-    color  = d3.scale.category10();
+var cValue = function(d) { return `${d.year} ${d.trim}` },
+    color  =  d3.scale.category20c()
+        .domain(['2012 touring', '2013 touring', '2014 touring', '2015 touring', '2012 touring elite', '2013 touring elite', '2014 touring elite', '2015 touring elite'])
 
 // add the graph canvas to the body of the webpage
 var svg = d3.select("body").append("svg")
@@ -41,9 +72,27 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+var table   = d3.select("body").append("table");
+var thead   = table.append("thead");
+var tbody   = table.append("tbody");
+var columns = [
+    { head : 'year', cl : '', html : r => r.year },
+    { head : 'trim', cl : '', html : r => r.trim },
+    { head : 'color', cl : '', html : r => r.color },
+    { head : 'miles', cl : '', html : r => r.miles },
+    { head : 'price', cl : '', html : r => r.price },
+    { head : 'history', cl : '', html : r => r.history },
+    { head : 'tags', cl : '', html : r => r.tags },
+    { head : 'dealer', cl : '', html : r => r.dealer },
+    { head : 'city', cl : '', html : r => r.city },
+    { head : 'state', cl : '', html : r => r.state },
+    { head : 'distance', cl : '', html : r => r.distance }
+];
+thead.append("tr").selectAll("th").data(columns).enter().append("th").attr('class', c => c.cl ).text( c => c.head );
+
 // set up slider for total expected miles
 (() => {
-    let margin = { top: 200, right: 50, bottom: 200, left: 50 },
+    let margin = { top: 200, right: 20, bottom: 200, left: 60 },
         width  = 960 - margin.left - margin.right,
         height = 500 - margin.bottom - margin.top;
     let x = d3.scale.linear()
@@ -64,6 +113,7 @@ var svg = d3.select("body").append("svg")
         updateY(value);
     }
 
+
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", `translate(0, ${500 + height/2})`)
@@ -71,8 +121,13 @@ var svg = d3.select("body").append("svg")
             .scale(x)
             .orient("bottom")
             .tickFormat(d => { return d })
-            .tickSize(0)
-            .tickPadding(12));
+            .tickPadding(12))
+       .append("text")
+         .attr("class", "label")
+         .attr("x", width)
+         .attr("y", -6)
+         .style("text-anchor", "end")
+         .text("Lifetime Milage");
 
     // add slider
     var slider = svg.append("g")
@@ -83,7 +138,7 @@ var svg = d3.select("body").append("svg")
     var handle = slider.append("circle")
         .attr("class", "handle")
         .attr("transform", `translate(0, ${500 + height/2})`)
-        .attr("r", 9);
+        .attr("r", 5);
 
     slider
         .call(brush.event)
@@ -105,11 +160,7 @@ var tooltip = d3.select("body").append("div")
 d3.csv("vans.csv", function(error, data) {
 
   // change string (from CSV) into number format
-  data.forEach(function(d) {
-    d.price = +d.price;
-    d.miles = +d.miles;
-    d.year  = +d.year;
-  });
+  data.forEach(process)
 
   // don't want dots overlapping axis, so add in buffer to data domain
   xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
@@ -137,7 +188,13 @@ d3.csv("vans.csv", function(error, data) {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Price");
+      .text("Price / year");
+
+  let r = d3.scale.linear()
+    .domain([75,5])
+    .range([1,10])
+    .clamp(true)
+
 
   // draw dots
   svg.selectAll(".dot")
@@ -147,7 +204,7 @@ d3.csv("vans.csv", function(error, data) {
       .attr("xlink:href", function(d) { return d.link })
     .append("circle")
       .attr("class", "dot")
-      .attr("r", 3.5)
+      .attr("r", car => r(car.distance))
       .attr("cx", xMap)
       .attr("cy", yMap)
       .style("fill", function(d) { return color(cValue(d)) })
@@ -155,10 +212,9 @@ d3.csv("vans.csv", function(error, data) {
           tooltip.transition()
                .duration(200)
                .style("opacity", .9);
-          tooltip.html(d.year + "<br/> (" + xValue(d)
-	        + ", " + yValue(d) + ")")
-               .style("left", (d3.event.pageX + 5 ) + "px")
-               .style("top",  (d3.event.pageY - 28) + "px");
+          tooltip.html(`${d.year} ${d.trim}<br/>$${d.price}<br/>${d.miles} miles`)
+               .style("left", (d3.event.pageX + 15 ) + "px")
+               .style("top",  (d3.event.pageY +10) + "px");
       })
       .on("mouseout", function(d) {
           tooltip.transition()
@@ -175,16 +231,34 @@ d3.csv("vans.csv", function(error, data) {
 
   // draw legend colored rectangles
   legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
+      .attr("x", width - 90)
+      .attr("width", 10)
       .attr("height", 18)
       .style("fill", color);
 
   // draw legend text
   legend.append("text")
-      .attr("x", width - 24)
+      .attr("x", width - 75)
       .attr("y", 9)
       .attr("dy", ".35em")
-      .style("text-anchor", "end")
+      .style("text-anchor", "start")
       .text(function(d) { return d })
+
+  // draw a table of data
+  var rows = tbody.selectAll("tr")
+    .data(data)
+    .enter()
+    .append("tr");
+
+  var cells = rows.selectAll("td")
+    .data( (r, i) => columns.map( c => {
+        var cell = {};
+        d3.keys(c).forEach(function(k) {
+            cell[k] = typeof c[k] == 'function' ? c[k](r,i) : c[k]
+        });
+        return cell;
+    } ))
+    .enter()
+    .append("td")
+      .html( d => d.html ).attr('class', d => d.cl );
 });
